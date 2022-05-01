9d1f399e26dcf8cf7a4feae0a982978eea71a81c
SeongJongHo <whdgh9595@gmail.com>
2022-05-01 18:18:17 +0900

    MODIFY: user-service 수정

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,12 +2,7 @@ const kakaoAPI = require('../middleware/kakaoAPI').kakaoAPI
 
 const kakaoSignIn = ()=>{
     try{
-        return {
-            token: token,
-            user_id: user.id,
-            user_nickname: user.nickname, 
-            user_profile_img: user.profile_img,
-        }
+
     }
     catch(err){
 
